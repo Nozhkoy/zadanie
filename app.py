0a5b616036119ad26ba64336ec95b1110d110c4e
i.ushatov <Ushatov4857@gmail.com>
2023-08-17 12:36:42 +0200

    qq

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "QQ!"
+
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you?'
+    return 'I am supergood and very well, how about you?'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
